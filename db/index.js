a1f39cab54ac7efb9242b47552161dc067e9baa9
M Raihan H <ar937464@gmail.com>
2026-09-03 12:00:52 +0700

    registrasi

--- db/index.js
+++ db/index.js
@@ -1,8 +1,7 @@
 import { Pool } from "pg";
-import { connectionString } from "pg/lib/defaults";
 
 // konfigutasi SSL dibawa oleh ?sslmode=no-verify di DAATABASE_URL
 // supaya cukup diset di satu tepat (file .env)
-export const Pool = new Pool({
+export const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 });
--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-import ''
+import 'dotenv/config';
+import { Hono } from "hono";
+import bcrypt from 'bcryptjs';
+import { pool } from './db/index.js';
+import { serve } from '@hono/node-server';
+
+const app = new Hono()
+
+// auth
+
+app.post('/api/register', async (c) => {
+    const { username, password } = await c.req.json();
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+        const { rows } = await pool.query(
+            'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id, username',
+            [username, hashedPassword]
+        );
+        return c.json({ succes: true, data: rows[0] }, 201);
+    } catch (err) {
+    console.error(err)
+    return c.json({ succes: false, massage: 'Registrasi gagal' }, 400);
+    }
+
+
+});
+
+// Ekspor app afar Vercel mengenalinya sebagai serverless handler
+   export default app;
+
+// jalankan server ini hanya di lingkungan lokal (bukan vercel)
+if (!process.env.VERCEL) {
+    const port = 3000;
+    console.log(`Server Berjalan Di "http://localhost:${port}`);
+    serve({ fetch: app.fetch, port });
+}
