109cf3f8d3eb3d75eb587846bd6647435a610285
M Raihan H <ar937464@gmail.com>
2026-09-03 12:20:13 +0700

    login

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import { Hono } from "hono";
 import bcrypt from 'bcryptjs';
 import { pool } from './db/index.js';
 import { serve } from '@hono/node-server';
+import jwt from 'jsonwebtoken';
+import { setCookie } from 'hono/cookie';
+import { getCookie, setCookie } from 'hono/cookie';
 
 const app = new Hono()
 
@@ -24,6 +27,31 @@ app.post('/api/register', async (c) => {
     }
 
 
+});
+
+app.post('/api/login', async (c) => {
+    const { username, password } = await c.req.json();
+
+    const { rows } = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+    [0];
+
+    if (!user) {
+        return c.json({ success: false, massage: 'Username atau password salah' }, 401);
+    }
+
+    const isPassowrdValid = await bcrypt.compare(password, user.password);
+    if (!isPassowrdValid) {
+        return c.json({ succes: false, massage: 'Username atau password salah' }, 401);
+    }
+
+    const token = jwt.sign(
+        { id: user.id, username: user.username },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+    );
+
+    setCookie(c, 'token', token, { httpOnly: true, sameSite: 'Lax', maxAge: 3600 });
+    return c.json({ success: true, massage: 'Login berhasil' });
 });
 
 // Ekspor app afar Vercel mengenalinya sebagai serverless handler
