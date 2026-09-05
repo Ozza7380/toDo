0938a283b7719becbe90923840cf11f54f4af7a7
M Raihan H <ar937464@gmail.com>
2026-09-05 11:12:14 +0700

    list todos

--- index.js
+++ index.js
@@ -94,6 +94,24 @@ app.post('/api/logout', (c) => {
     return c.json({ success: true, massage: 'Logout berhasil' })
 });
 
+//menampilkan note
+app.get('/api/todos', async (c) => {
+    const token = getCookie(c, 'token');
+    if (!token) return c.json({ success: false, massage: 'Unauthorize' }, 401);
+
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        const { rows } = await pool.query(
+            'SELECT id, note, user_id FROM todos WHERE user_id =$1 ORDER BY id',
+            [user.id]
+        );
+        return c.json({ success: true, data: rows });
+    } catch (err) {
+        console.error(err);
+        return c.json({ success: false, massage: 'Server error' })
+    }
+});
+
 // Ekspor app afar Vercel mengenalinya sebagai serverless handler
    export default app;
 
